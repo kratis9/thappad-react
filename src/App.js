42230c31d1431d04161c00803c2caf1d2c269e84
kratis9 <kratis9@gmail.com>
2020-08-18 14:17:14 +1000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./styles.css";
+
+class Mumma extends React.Component {
+  constructor(props) {
+    super(props);
+    this.childInDanger = this.childInDanger.bind(this);
+  }
+
+  childInDanger(reason) {
+    console.log(reason);
+  }
+
+  render() {
+    return (
+      <>
+        <h1>{this.props.message}</h1>
+        <Mittu onHurt={this.childInDanger} />
+      </>
+    );
+  }
+}
+
+class Mittu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleHurt = this.handleHurt.bind(this);
+  }
+  handleHurt() {
+    this.props.onHurt("tappad");
+  }
+
+  render() {
+    return <h1 onClick={this.handleHurt}>hey i am mittu</h1>;
+  }
+}
+
+export default function App() {
+  return (
+    <div className="App">
+      <Mumma />
+    </div>
+  );
+}
